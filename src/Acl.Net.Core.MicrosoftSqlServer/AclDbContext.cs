1809bb4ce3473b01549c02b29a6efc3e2f927d11
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-06-30 17:43:54 +0300

    Add project Acl.Net.Core.MicrosoftSqlServer

---
+++ src/Acl.Net.Core.MicrosoftSqlServer/AclDbContext.cs
@@ -0,0 +1,71 @@
+﻿using Acl.Net.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Acl.Net.Core.MicrosoftSqlServer;
+
+public class AclDbContext : AclDbContext<User>
+{
+    protected AclDbContext() { }
+
+    public AclDbContext(DbContextOptions options) : base(options) { }
+}
+
+public class AclDbContext<TUser> : AclDbContext<TUser, Role, Resource, Permission>
+    where TUser : User
+{
+    protected AclDbContext() { }
+
+    public AclDbContext(DbContextOptions options) : base(options) { }
+}
+
+public class AclDbContext<TUser, TRole, TResource, TPermission> : DbContext
+    where TUser : User
+    where TRole : Role
+    where TResource : Resource
+    where TPermission : Permission
+{
+    protected AclDbContext() { }
+    public AclDbContext(DbContextOptions options) : base(options) { }
+
+    public virtual DbSet<TUser> Users { get; set; } = null!;
+    public virtual DbSet<TRole> Roles { get; set; } = null!;
+    public virtual DbSet<TResource> Resources { get; set; } = null!;
+    public virtual DbSet<TPermission> Permissions { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<TUser>(entity =>
+        {
+            entity.HasKey(u => u.Id);
+            entity.Property(u => u.UserId).IsRequired();
+            entity.Ignore(u => u.RoleNames);
+
+            entity.HasMany(u => u.Roles).WithOne().HasForeignKey(ut => ut.UserId).IsRequired();
+        });
+
+        modelBuilder.Entity<TRole>(entity =>
+        {
+            entity.HasKey(r => r.Id);
+            entity.Property(r => r.Name).IsRequired();
+            entity.Ignore(r => r.Parents);
+
+            entity.HasMany(r => r.Resources).WithOne().HasForeignKey(res => res.RoleId).IsRequired();
+        });
+
+        modelBuilder.Entity<TResource>(entity =>
+        {
+            entity.HasKey(r => r.Id);
+            entity.Property(r => r.Name).IsRequired();
+
+            entity.HasMany(r => r.Permissions).WithOne().HasForeignKey(p => p.ResourceId).IsRequired();
+        });
+
+        modelBuilder.Entity<TPermission>(entity =>
+        {
+            entity.HasKey(p => p.Id);
+            entity.Property(p => p.Name).IsRequired();
+        });
+    }
+}
---
+++ src/Acl.Net.Core.MicrosoftSqlServer/MsSql.cs
@@ -0,0 +1,99 @@
+﻿using Acl.Net.Core.Entities;
+using Acl.Net.Core.Providers;
+using Microsoft.EntityFrameworkCore;
+
+namespace Acl.Net.Core.MicrosoftSqlServer;
+
+public class MsSql : MsSql<User>
+{
+    public MsSql(AclDbContext context)
+        : base(context)
+    { }
+}
+
+public class MsSql<TUser> : MsSql<TUser, Role, Resource, Permission>
+    where TUser : User
+{
+    public MsSql(AclDbContext<TUser> context)
+        : base(context)
+    { }
+}
+
+public class MsSql<TUser, TRole, TResource, TPermission> : Backend<TUser, TRole>
+    where TUser : User
+    where TRole : Role
+    where TResource : Resource
+    where TPermission : Permission
+{
+    private readonly AclDbContext<TUser, TRole, TResource, TPermission> _context;
+
+    public MsSql(AclDbContext<TUser, TRole, TResource, TPermission> context)
+    {
+        _context = context;
+    }
+
+    public override TRole CreateRole(TRole role)
+    {
+        _context.Roles.Add(role);
+        _context.SaveChanges();
+        return role;
+    }
+
+    public override TRole? GetRole(string roleName) =>
+        _context.Roles.Include(r => r.Resources).SingleOrDefault(r => r.Name == roleName);
+
+    public override TRole UpdateRole(TRole role)
+    {
+        _context.Roles.Update(role);
+        _context.SaveChanges();
+        return role;
+    }
+
+    public override void DeleteRole(TRole role)
+    {
+        var toDelete = _context.Roles.SingleOrDefault(r => r.Id == role.Id);
+        if (toDelete == null) return;
+        _context.Roles.Remove(toDelete);
+        _context.SaveChanges();
+    }
+
+    public override TUser CreateUser(TUser user)
+    {
+        _context.Users.Add(user);
+        _context.SaveChanges();
+        return user;
+    }
+
+    public override TUser? GetUser(string userId) =>
+        _context.Users.SingleOrDefault(u => u.UserId == userId);
+
+    public override TUser[] GetUsers(string roleName) =>
+        _context.Users.Where(u => u.RoleNames.Contains(roleName)).ToArray();
+
+    public override TUser UpdateUser(TUser user)
+    {
+        _context.Users.Update(user);
+        _context.SaveChanges();
+        return user;
+    }
+
+    public override void DeletePermission(string permissionName)
+    {
+        var roles = _context.Roles.Include(r => r.Resources).ToList();
+        foreach (var resource in roles.SelectMany(role => role.Resources))
+        {
+            resource.Permissions = resource.Permissions.Where(p => p.Name != permissionName).ToArray();
+        }
+        _context.SaveChanges();
+    }
+
+    public override void DeleteResource(string resourceName)
+    {
+        var roles = _context.Roles.Include(r => r.Resources).ToList();
+        foreach (var role in roles)
+        {
+            role.Resources = role.Resources.Where(r => r.Name != resourceName).ToArray();
+        }
+        _context.SaveChanges();
+    }
+}
