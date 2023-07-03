f8852dabdee1b7a07ecbd72575c64f7ec4a21292
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-07-03 14:04:41 +0300

    remove UserId from User model

--- src/Acl.Net.Core/Entities/User.cs
+++ src/Acl.Net.Core/Entities/User.cs
@@ -9,14 +9,6 @@ public class User
 
     public Guid Id { get; set; }
 
-    public string UserId { get; set; } = null!;
-
     public virtual ICollection<Role> Roles { get; set; } = Array.Empty<Role>();
     public virtual ICollection<Claim> Claims { get; set; } = Array.Empty<Claim>();
-
-    /// <summary>
-    /// Returns the <see cref="UserId"/> for this user.
-    /// </summary>
-    public override string ToString() =>
-        string.IsNullOrWhiteSpace(UserId) ? UserId : string.Empty;
 }
