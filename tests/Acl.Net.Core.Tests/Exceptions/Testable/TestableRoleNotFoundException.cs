69375bf37db17f816912dbfb64e060cab79814bb
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-07-03 13:55:44 +0300

    add tests for exceptions

---
+++ tests/Acl.Net.Core.Tests/Exceptions/Testable/TestableRoleNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using Acl.Net.Core.Exceptions;
+using System.Runtime.Serialization;
+
+namespace Acl.Net.Core.Tests.Exceptions.Testable;
+
+public class TestableRoleNotFoundException : RoleNotFoundException
+{
+    public TestableRoleNotFoundException(SerializationInfo info, StreamingContext context)
+        : base(info, context)
+    { }
+}
---
+++ tests/Acl.Net.Core.Tests/Exceptions/UserNotFoundExceptionTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+using Acl.Net.Core.Exceptions;
+using System.Runtime.Serialization;
+using Acl.Net.Core.Tests.Exceptions.Testable;
+
+namespace Acl.Net.Core.Tests.Exceptions;
+
+public class UserNotFoundExceptionTests : ExceptionSerializationTestBase<UserNotFoundException, TestableUserNotFoundException>
+{
+    [Fact]
+    internal void GasPriceExceededException_SerializationTest()
+    {
+        const string expectedMessage = "exception message";
+
+        RunSerializationTest(expectedMessage);
+
+        var actualMessage = infoForGetObjectData.GetString("Message");
+        Assert.Equal(expectedMessage, actualMessage);
+    }
+
+    protected override TestableUserNotFoundException CreateTestableException(SerializationInfo info, StreamingContext context) =>
+        new(info, context);
+}
