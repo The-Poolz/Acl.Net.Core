fc7964682332ba9f3f188b474196c42cf9466e6c
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-07-06 16:18:32 +0300

    add one test

---
+++ tests/Acl.Net.Core.Tests/DataProvider/AclDbContextTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+using Acl.Net.Core.DataProvider;
+
+namespace Acl.Net.Core.Tests.DataProvider;
+
+public class AclDbContextTests
+{
+    [Fact]
+    public void FirstConstructorTest()
+    {
+        var context = new AclDbContext();
+
+        Assert.NotNull(context);
+    }
+}
