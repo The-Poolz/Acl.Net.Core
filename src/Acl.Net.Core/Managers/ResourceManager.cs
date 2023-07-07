6ec1fbcfae213bb1b457069c52adb0e3832433de
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-07-07 12:11:12 +0300

    add UserManager and ResourceManager

---
+++ src/Acl.Net.Core/Managers/ResourceManager.cs
@@ -0,0 +1,33 @@
+﻿using Acl.Net.Core.Entities;
+using Acl.Net.Core.DataProvider;
+using Microsoft.EntityFrameworkCore;
+
+namespace Acl.Net.Core.Managers;
+
+public class ResourceManager<TKey, TUser, TRole, TResource>
+    where TKey : IEquatable<TKey>
+    where TUser : User<TKey>, new()
+    where TRole : Role<TKey>
+    where TResource : Resource<TKey>
+{
+    private readonly AclDbContext<TKey, TUser, TRole, TResource> context;
+    private readonly IInitialDataSeeder<TKey, TRole> initialDataSeeder;
+
+    public ResourceManager(AclDbContext<TKey, TUser, TRole, TResource> context, IInitialDataSeeder<TKey, TRole> initialDataSeeder)
+    {
+        this.context = context;
+        this.initialDataSeeder = initialDataSeeder;
+    }
+
+    public bool IsPermitted(TUser user, TResource resource)
+    {
+        return user.RoleId.Equals(initialDataSeeder.SeedAdminRole().Id) ||
+            context.Resources.Any(r => r.RoleId.Equals(user.RoleId) && r.Id.Equals(resource.Id));
+    }
+
+    public async Task<bool> IsPermittedAsync(TUser user, TResource resource)
+    {
+        return user.RoleId.Equals(initialDataSeeder.SeedAdminRole().Id) ||
+            await context.Resources.AnyAsync(r => r.RoleId.Equals(user.RoleId) && r.Id.Equals(resource.Id));
+    }
+}
