ffbf9358b23f88268047e24676ef5d3890be6773
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-08-21 19:52:15 +0300

    update AclManagerTests

--- tests/Acl.Net.Core.Tests/Mock/InMemoryAclDbContext.cs
+++ tests/Acl.Net.Core.Tests/Mock/InMemoryAclDbContext.cs
@@ -10,6 +10,36 @@ internal static class InMemoryAclDbContext
     private static readonly Role<int> userRole = roleDataSeeder.SeedUserRole();
     private static readonly Role<int> adminRole = roleDataSeeder.SeedAdminRole();
 
+    internal static User UserAccount = new()
+    {
+        Id = 1, Name = "UserAccount", RoleId = userRole.Id
+    };
+
+    internal static User AdminAccount = new()
+    {
+        Id = 2,
+        Name = "AdminAccount",
+        RoleId = adminRole.Id
+    };
+
+    internal static Resource PublicResource = new()
+    {
+        Id = 1,
+        Name = "PublicResource",
+        RoleId = userRole.Id
+    };
+
+    internal static Resource PrivateResource = new()
+    {
+        Id = 2,
+        Name = "PrivateResource",
+        RoleId = adminRole.Id
+    };
+
+    internal static List<User> Users => new() { UserAccount, AdminAccount };
+
+    internal static List<Resource> Resources => new() { PublicResource, PrivateResource };
+
     internal static AclDbContext CreateContext()
     {
         var options = new DbContextOptionsBuilder<AclDbContext>()
@@ -25,16 +55,4 @@ internal static class InMemoryAclDbContext
 
         return context;
     }
-
-    internal static List<User> Users => new()
-    {
-        new User { Id = 1, Name = "UserAccount", RoleId = userRole.Id},
-        new User { Id = 2, Name = "AdminAccount", RoleId = adminRole.Id}
-    };
-
-    internal static List<Resource> Resources => new()
-    {
-        new Resource { Id = 1, Name = "PublicResource", RoleId = userRole.Id },
-        new Resource { Id = 2, Name = "PrivateResource", RoleId = adminRole.Id }
-    };
 }
