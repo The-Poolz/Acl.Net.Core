12dcb8bb76a91ff466781b8b7bb45a0577dddd7e
Stanislav Vysotskyi <ardenhide@gmail.com>
2024-11-22 12:56:16 +0300

    - downgrade version of EFCore at DB package - update interface IAclmanager - update `IsPermitted(TRole role, TResource resource)`: check by resource name, not by Id

--- src/Acl.Net.Core.Managers/AclManager.cs
+++ src/Acl.Net.Core.Managers/AclManager.cs
@@ -5,8 +5,8 @@ using Acl.Net.Core.Managers.Exceptions;
 namespace Acl.Net.Core.Managers;
 
 /// <summary>
-/// Manages access control lists (ACLs) using integer keys.
-/// This class provides a simplified interface for managing ACLs with integer keys, by extending the more generic <see cref="AclManager{TKey}"/> with TKey.
+/// Manages access control lists (ACLs) using integer keys.<br/>
+/// This class provides a simplified interface for managing ACLs with integer keys, by extending the more generic <see cref="AclManager{TKey}"/>.
 /// </summary>
 public class AclManager : AclManager<int>, IAclManager
 {
@@ -35,7 +35,7 @@ public class AclManager : AclManager<int>, IAclManager
 }
 
 /// <summary>
-/// Manages access control lists (ACLs) using keys of type <see cref="TKey"/>.
+/// Manages access control lists (ACLs) using keys of type <see cref="TKey"/>.<br/>
 /// This class provides the base functionality for managing ACLs with specified key types.
 /// </summary>
 /// <typeparam name="TKey">The type of the key, which must implement <see cref="IEquatable{TKey}"/>.</typeparam>
@@ -57,14 +57,14 @@ public class AclManager<TKey> : AclManager<TKey, User<TKey>, Role<TKey>, Resourc
 }
 
 /// <summary>
-/// Manages access control lists (ACLs) using keys, users, roles, and resources of specified types.
+/// Manages access control lists (ACLs) using keys, users, roles, and resources of specified types. <br/>
 /// This class provides the complete functionality for managing ACLs with specified key, user, role, and resource types.
 /// </summary>
 /// <typeparam name="TKey">The type of the key, which must implement <see cref="IEquatable{TKey}"/>.</typeparam>
 /// <typeparam name="TUser">The type of the user, which must be a derived type of <see cref="User{TKey}"/>.</typeparam>
 /// <typeparam name="TRole">The type of the role, which must be a derived type of <see cref="Role{TKey}"/></typeparam>
 /// <typeparam name="TResource">The type of the resource, which must be a derived type of <see cref="Resource{TKey}"/></typeparam>
-public class AclManager<TKey, TUser, TRole, TResource> : IAclManager<TKey, TUser, TResource>
+public class AclManager<TKey, TUser, TRole, TResource> : IAclManager<TKey, TUser, TRole, TResource>
     where TKey : IEquatable<TKey>
     where TUser : User<TKey>, new()
     where TRole : Role<TKey>
@@ -88,20 +88,23 @@ public class AclManager<TKey, TUser, TRole, TResource> : IAclManager<TKey, TUser
         Context = context;
     }
 
+    /// <inheritdoc />
     public bool IsPermitted(string userName, string resourceName)
     {
         var user = GetUserByName(userName);
         return IsAdmin(user) || GetUserRoles(user.Name).Any(role => IsPermitted(role, GetResourceByName(resourceName)));
     }
 
+    /// <inheritdoc />
     public bool IsPermitted(TUser user, TResource resource)
     {
         return IsAdmin(user) || GetUserRoles(user.Name).Any(role => IsPermitted(role, resource));
     }
 
+    /// <inheritdoc />
     public bool IsPermitted(TRole role, TResource resource)
     {
-        return IsAdmin(role) || Context.Resources.Any(r => r.RoleId.Equals(role.Id) && r.Id.Equals(resource.Id));
+        return IsAdmin(role) || Context.Resources.Any(res => res.Name.Equals(resource.Name) && res.RoleId.Equals(role.Id));
     }
 
     public bool IsAdmin(TKey roleId) => roleId.Equals(InitialDataSeeder.SeedAdminRole().Id);
